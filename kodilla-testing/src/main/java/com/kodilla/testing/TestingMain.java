c0397acb35a96bdf8b45f56d37473d469df41147
MadDwarfs <jakubmon0@gmail.com>
2020-06-12 15:37:14 +0200

    Calculator test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,38 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Test - pierwszy test jednostkowy:");
+
+        Calculator calculator = new Calculator();
+        int calculatorA = 12;
+        int calculatorB = 5;
+
+        if(calculator.add(calculatorA,calculatorB)==calculatorA+calculatorB){
+            System.out.println("Test successful");
+        } else {
+            System.out.println("Error!");
+        }
+
+        if(calculator.substract(calculatorA,calculatorB)==calculatorA-calculatorB){
+            System.out.println("Test successful");
+        } else {
+            System.out.println("Error!");
+        }
+
     }
+
 }
